845021a3a1385f67d0859f7c67fa2e2206001179
Mike Lee <mike.lee816@gmail.com>
2022-06-17 21:26:44 -0400

    Create MenuBar component

---
+++ components/menu-bar.component.tsx
@@ -0,0 +1,18 @@
+import styles from '../styles/MenuBar.module.scss';
+
+import MenuIcon from './menu-icon.component';
+
+interface Props {
+    setNavVisible: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+const MenuBar: React.FC<Props> = ({ setNavVisible }) => (
+    <div className={styles['menu-bar']}>
+        <h1 className={styles['name']}>Sample</h1>
+        <button className={styles['menu-button']} onClick={() => setNavVisible(true)}>
+            <MenuIcon size={60} />
+        </button>
+    </div>
+);
+
+export default MenuBar;
